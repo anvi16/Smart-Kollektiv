2df0bcfaef4541e1e96500e4a67080baba70a6d9
Andreas Vik <swews92@gmail.com>
2021-04-11 22:54:47 +0200

    Delete Programmer/ESP32_Entry/Debug directory

--- Programmer/ESP32_Entry/Debug/Adafruit_GFX_Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\andre\AppData\Local\Temp\VMBuilds\ESP32_Entry\esp32_esp32\Debug\Adafruit_GFX_Library\glcdfont.c.o: \
- C:\Users\andre\Dropbox\Apps\ArduinoDroid\Arduino\libraries\Adafruit_GFX_Library\glcdfont.c

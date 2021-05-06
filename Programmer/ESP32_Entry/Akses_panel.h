a78d490fa9b51ac70bb151bbafb723f9c3afbf7b
TankBastian <74309100+TankBastian@users.noreply.github.com>
2021-05-06 18:44:46 +0200

    Update Akses_panel.h

--- Programmer/ESP32_Entry/Akses_panel.h
+++ Programmer/ESP32_Entry/Akses_panel.h
@@ -4,190 +4,263 @@
    Smart_kollektiv: Acsses.
 */
 
-//#define DEBUG  // <-- Define for debug 
+// Kort leser
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN 21
+#define RST_PIN 22
+MFRC522 rfid(SS_PIN, RST_PIN);
 
-#include<CircusESP32Lib.h>
-
-char ssid[]        = "Telenor6619dyp";
-char password[]    = "josehome08";
-char server[]      = "www.circusofthings.com";
-char token[]       = "LED";
-char User1_Key_1[] = "6442";
-char User1_Key_2[] = "30385";
-char User1_Key_3[] = "8691";
-char User1_Key_4[] = "21957";
-
-CircusESP32Lib circusESP32(server,ssid,password);
-
-    
-int button_pin0 = 2;
-int button_pin1 = 3;
-int button_pin2 = 4;
-int button_pin3 = 5;
-int button_pin4 = 6;
-int button_pin5 = 7;
-int button_pin6 = 8;
-int button_pin7 = 9;
-int button_pin8 = 10;
-int button_pin9 = 11;
-
-int button_pinEnter = 12;
-int button_pinRemove = 13;
-
-int      button_delay      = 30;   // 30ms delay
+// Keypad
+#include <Keypad.h>
+const byte ROWS = 4;
+const byte COLS = 3;
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
+};
+byte rowPins[ROWS] = { 32, 12, 9, 27 };
+byte colPins[COLS] = { 26, 25, 33 };
+int      button_delay = 30;   // 30ms delay
 int      button_menu_delay = 2000; // 2sek delay
-uint32_t button_press_last = 0;
-int      last_pressed;
-bool     last_read_press;
 
 
-int user1_val_1, user1_val_2, user1_val_3, user1_val_4;
-
+String new_card;
+String code;
 int key_nr;
-int code[4];
-int access_code1[4] = { 1, 3, 3, 4 };
-int access_code2[4] = { 1, 2, 3, 4 };
-int access_code3[4] = { 1, 0, 0, 0 };
-int access_code4[4] = { 4, 3, 2, 1 };
-int access_code5[4] = { 1, 0, 0, 0 };
-int access_code6[4] = { 1, 0, 0, 0 };
 
-//int access_code1[4] = {user1_val_1, user1_val_2, user1_val_3, user1_val_4} ;
+bool spesial_menu_check = false;
+int new_user_id;
+String new_user_item;
 
 
-void setup() {
-    Serial.begin(9600);
-    pinMode(button_pin0, INPUT_PULLUP);
-    pinMode(button_pin1, INPUT_PULLUP);
-    pinMode(button_pin2, INPUT_PULLUP);
-    pinMode(button_pin3, INPUT_PULLUP);
-    pinMode(button_pin4, INPUT_PULLUP);
-    pinMode(button_pin5, INPUT_PULLUP);
-    pinMode(button_pin6, INPUT_PULLUP);
-    pinMode(button_pin7, INPUT_PULLUP);
-    pinMode(button_pin8, INPUT_PULLUP);
-    pinMode(button_pin9, INPUT_PULLUP);
-    pinMode(button_pinEnter,  INPUT_PULLUP);
-    pinMode(button_pinRemove, INPUT_PULLUP);
+Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+#define users 6
 
-    circusESP32.begin();
+
+// Servo
+#include <ESP32Servo.h>
+Servo myservo;
+int servoPin = 13;
+int servo_door_opne_delay = 5000;
+long unsigned int now_time;
+
+
+
+//Kryptering og MQTT
+#include "mbedtls/md.h"; // 
+String user_card[users];
+String user_code[users] = { "3e8a85792028bdeeb97650328ac3394a32eab166740c08808f603f0541517e2a",
+                            "3e8a85792028bdeeb97650328ac3394a32eab166740c08808f603f0541517e2a",
+                            "3e8a85792028bdeeb97650328ac3394a32eab166740c08808f603f0541517e2a",
+                            "3e8a85792028bdeeb97650328ac3394a32eab166740c08808f603f0541517e2a",
+                            "3e8a85792028bdeeb97650328ac3394a32eab166740c08808f603f0541517e2a",
+                            "3e8a85792028bdeeb97650328ac3394a32eab166740c08808f603f0541517e2a"
+                          };
+
+String Akses_panel_sha256(String tolk) {
+  char* key = "SD3wGw&teRy^!GYiY24Q2eqZ";
+  char* payload = const_cast<char*>(tolk.c_str());
+  byte hmacResult[32];
+
+  mbedtls_md_context_t ctx;
+  mbedtls_md_type_t md_type = MBEDTLS_MD_SHA256;
+
+  const size_t payloadLength = strlen(payload);
+  const size_t keyLength = strlen(key);
+
+  mbedtls_md_init(&ctx);
+  mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(md_type), 1);
+  mbedtls_md_hmac_starts(&ctx, (const unsigned char*)key, keyLength);
+  mbedtls_md_hmac_update(&ctx, (const unsigned char*)payload, payloadLength);
+  mbedtls_md_hmac_finish(&ctx, hmacResult);
+  mbedtls_md_free(&ctx);
+  String sha256;
+  for (int i = 0; i < sizeof(hmacResult); i++) {
+    char str[3];
+
+    sprintf(str, "%02x", (int)hmacResult[i]);
+    sha256 += str;
+  }
+  return sha256;
 }
-
-void loop() {
-    user1_val_1 = circusESP32.read(User1_Key_1, token);
-    user1_val_2 = circusESP32.read(User1_Key_2, token);
-    user1_val_3 = circusESP32.read(User1_Key_3, token);
-    user1_val_4 = circusESP32.read(User1_Key_4, token);
-
-    bool b0 = !digitalRead(button_pin0);
-    bool b1 = !digitalRead(button_pin1);
-    bool b2 = !digitalRead(button_pin2);
-    bool b3 = !digitalRead(button_pin3);
-    bool b4 = !digitalRead(button_pin4);
-    bool b5 = !digitalRead(button_pin5);
-    bool b6 = !digitalRead(button_pin6);
-    bool b7 = !digitalRead(button_pin7);
-    bool b8 = !digitalRead(button_pin8);
-    bool b9 = !digitalRead(button_pin9);
-
-    bool bE = !digitalRead(button_pinEnter);
-    bool bC = digitalRead(button_pinRemove);
-
-
-    bool Code_box[] = { b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, bE, bC };
-
-    for (int i = 0; i < 12; i++) {
-        if (Code_box[i] && !last_read_press && button_press_last + button_delay < millis()) {  // Check's for debounse and if button is not pressed long
-            button_press_last = millis();
-            last_pressed = i;
-            last_read_press = true;
-            if (i < 10 && key_nr < 4) { // Check's if butten is 0-9 and pressed four times
-                #ifdef DEBUG
-                    Serial.println(i);
-                #endif
-                code[key_nr] = i;
-                key_nr++;
-                break;
-            }
-            else if (bC) {
-                code[0] = 0;
-                code[1] = 0;
-                code[2] = 0;
-                code[3] = 0;
-                key_nr = 0;
-                #ifdef DEBUG
-                    Serial.println("Clear");
-                #endif
-            }
-            else if (bE) {
-                bool accept_the_code = false;
-                if      (Enter(access_code1)) accept_the_code = true;
-                else if (Enter(access_code2)) accept_the_code = true;
-                else if (Enter(access_code3)) accept_the_code = true;
-                else if (Enter(access_code4)) accept_the_code = true;
-                else if (Enter(access_code5)) accept_the_code = true;
-                else if (Enter(access_code6)) accept_the_code = true;
-
-                if (accept_the_code) {
-                    Serial.println("LE GOW");
-                }
-                else {
-                    Serial.println("Nope");
-                }
-                key_nr = 0;
-            }
-        }
-        else {
-            if (last_pressed == 0 && button_press_last + button_menu_delay < millis()) {
-                button_press_last = millis();
-                // Reset all
-                code[0] = 0;
-                code[1] = 0;
-                code[2] = 0;
-                code[3] = 0;
-                key_nr = 0;
-
-                // Go to spesial menu //
-                Serial.println("Spesial menu");
-                ////////////////////////
-            }
-            else if (!Code_box[last_pressed]) {     // Check if button pressed is released
-                 last_read_press = false;
-            }
-        }
-    }
+int Akses_panel_check_password() {
+  new_card = Akses_panel_sha256(new_card);
+  code = Akses_panel_sha256(code);          // Convert code to sha256 hash
+  for (int i = 0; i < users; i++) {
+    if      (new_card == user_card[i]) return i;
+    else if (code  == user_code[i]) return i;
+    else if (i == users - 1)  return 0;
+  }
 }
 
 
-bool Enter(int user_code[]) {
-    int check = 0;
+
+
+
+void setup()
+{
+  // Keypad
+  Serial.begin(115200);
+  keypad.addEventListener(Akses_panel_keypad_event);       // Add an event listener.
+  keypad.setHoldTime(button_menu_delay);
+  keypad.setDebounceTime(button_delay);
+  // NFC scanner
+  SPI.begin();
+  rfid.PCD_Init();
+
+  // Servo
+  myservo.setPeriodHertz(50);
+  myservo.attach(servoPin, 500, 2400);
+}
+
+
+void loop()
+{
+
+
+
+
+
+  char key = keypad.getKey();
+  if (rfid.PICC_IsNewCardPresent())
+  {
+    read_card();
+  }
+}
+
+
+
+
+
+
+
+void read_card()
+{
+  if (rfid.PICC_ReadCardSerial()) {
+    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
     for (int i = 0; i < 4; i++) {
-        if (code[i] == user_code[i]) {
-            check++;
-            if (check == 4) {
-                code[0] = 0;
-                code[1] = 0;
-                code[2] = 0;
-                code[3] = 0;
-                return 1;
-            }
-        }
-        else {
-            return 0;
-        }
+      new_card += String(rfid.uid.uidByte[i], HEX);
+
     }
+    rfid.PICC_HaltA();
+    rfid.PCD_StopCrypto1();
+  }
 }
 
-/*
-Hei, har godt over koden og lagt til en debounser (for � fjerne delay()'en) og en sjekk om knappene er holdt inne, 
-m�tte flytte litt p� deler av koden for � f� det til. 
-Har ikke endret koden din noe mer en og fjrnet noen f� overfl�dige variabler og fasongen (API'en gj�r mye endringer selv), 
-h�per du ser at koden er den samme. Du m� gjerne ender tilbake det du skulle f�le er n�dvndig (utsene messig/ eventuelt annet)
 
-Lagt klart til � kunne lagre nye koder og kort med adgang til en spesial meny.
 
-Det m� gjerne legges til litt komentarer p� koden, underveis og ikke n�l med � pr�ve og ta kontakt p� discord
-om du lurer p� noe, eller lurer p� hvordan det kan v�re lurt � legge inn oppdateringen av nye koder/kort mot rasperry pi.
-(Kan anbefale og ikke g� gjennom CoT)
+// Tall koden blir gennerert her.
+void Akses_panel_keypad_event(char key_press) {
+  if (spesial_menu_check == true)
+  {
+    Spesial_Menu(key_press);
+  }
+  else {
+    switch (keypad.getState()) {
+      case PRESSED:
+        make_a_code(key_press);
+        break;
 
-*/
+      case HOLD:
+        if (key_press == '0')
+        {
+          spesial_menu_check = true;
+          code = "";
+        }
+        break;
+    }
+  }
+}
+void Open_Door()
+{
+  now_time = millis();
+  myservo.write(180);
+  if ((millis() - now_time) >= servo_door_opne_delay /* && magnet_on_door_is_closed */ )
+  {
+    myservo.write(0);
+  }
+}
+
+
+void Spesial_Menu (char key_press)
+{
+  enum Menu {Item, Authenticate, New_code_card, Send_tolk};
+  Menu menu = Item;
+
+  switch (menu)
+  {
+    case Item:
+      if (key_press == '1')
+      {
+        new_user_item= "card";
+        menu = Authenticate;
+      }
+      else if (key_press == '2')
+      {
+        new_user_item = "code";
+        menu = Authenticate;
+      }
+      break;
+      
+    case Authenticate:
+      if (make_a_code(key_press))
+      {
+        menu = New_code_card;
+        code = "";
+        key_nr = 0;
+      }
+      break;
+      
+    case New_code_card:
+      if (make_a_code(key_press))
+      {
+        menu = Send_tolk;
+      }
+        
+      break;
+    case Send_tolk:
+      if (new_user_item == "card"){
+    // Send tolk Access_panel_push_tolk(new_user_id, new_user_item, new_card);
+      }
+      else if (new_user_item == "code"){
+    // Send tolk Access_panel_push_tolk(new_user_id, new_user_item, "", code);
+      }
+      break;
+  }
+}
+
+bool make_a_code(char key_press)
+{
+  if (key_press == '#') {
+    code = "";
+    key_nr = 0;
+  }
+  else if (key_press == '*') {
+    new_user_id = Akses_panel_check_password();
+    if (new_user_id != 0) {
+      if (!spesial_menu_check)
+      {
+        Open_Door();
+        Serial.println("LE GOW");
+        code = "";
+        key_nr = 0;
+      }
+      return true;
+    }
+    else
+    {
+      Serial.println("Nope");
+    }
+    code = "";
+    key_nr = 0;
+  }
+  else if (key_nr < 10) {
+    code += key_press;
+    key_nr++;
+
+  }
+  return false;
+}
